1f13ae46cab7ded8d9d7b9f269f997b7a20009b3
Mehdi Kahouadji <mehdi.kahouadji@gmail.com>
2016-08-19 16:03:29 +0200

    Update __init__.py

--- pythonplugins/plugins/plugin1/__init__.py
+++ pythonplugins/plugins/plugin1/__init__.py
@@ -5,7 +5,7 @@ class Plugin1(threading.Thread):
 
     def __init__(self):
         threading.Thread.__init__(self)
-        self.enabled = env.config.get('plugin1', 'enabled')
+        self.enabled = env.config.getboolean('plugin1', 'enabled')
 
     def run(self):
         print 'starting plugin1'
