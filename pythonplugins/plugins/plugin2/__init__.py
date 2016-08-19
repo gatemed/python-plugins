581587a549168e2d7cd29424abdfabbbd4ab5796
Mehdi Kahouadji <mehdi.kahouadji@gmail.com>
2016-08-19 11:16:39 +0200

    Create __init__.py for plugin2

---
+++ pythonplugins/plugins/plugin2/__init__.py
@@ -0,0 +1,11 @@
+import threading
+from pythonplugins import env
+
+class Plugin2(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.enabled = env.config.get('plugin2', 'enabled')
+
+    def run(self):
+        print 'starting plugin2'
